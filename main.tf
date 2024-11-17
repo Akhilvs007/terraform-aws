f7838bffbcccb4706bd3752bf3d9d9cddaea0bfa
Akhil VS <akhil.vs7@gmail.com>
2024-11-17 21:09:50 +0530

    initial tf file

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "3.38"
+      }
+    }
+    required_version = "1.9.8"
+}
+
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "my_nasty_server" {
+  ami = "ami-013d21c7f48ae9ff1"
+  instance_type = "t2.micro"
+}
